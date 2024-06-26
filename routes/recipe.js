ea8b3cb0e306cf2dbc9cdfcced11ed6cd5ac25ab
Sarah <saradarko22@gmail.com>
2024-06-26 16:16:14 +0000

    Express middleware

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { RecipeModel } from "../models/recipe.js";
 
 //Create a router
 
@@ -11,8 +12,12 @@ recipeRouter.get('/recipes',(req,res) =>{
 });
 
 
-recipeRouter.post('/recipes',(req,res) =>{
-    res.json('Recipe added');
+
+recipeRouter.post('/recipes',async (req,res) =>{
+    //Add recipe to database
+    await RecipeModel.create(req.body);
+    //return response
+    res.json("recipe added");
 });         
 
 recipeRouter.patch('/recipes/:id',(req,res) =>{
@@ -22,6 +27,12 @@ recipeRouter.patch('/recipes/:id',(req,res) =>{
 
 recipeRouter.delete('/recipes/:id',(req,res) =>{
     res.json(`Recipe with ID ${req.params.id} deleted`);
+});
+
+
+
+recipeRouter.get('/recipes/:id',(req,res)=>{
+    res.json(`Get recipe with ID ${req.params.id}`);
 });
 // export router
 
